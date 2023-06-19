0a6788ef4d3db9d2b79aac1646f64b306fc5b7a1
MikaZ21 <zukeyamamika@gmail.com>
2023-06-19 12:08:53 -0500

    Step002: Change the greeting

--- SimpleCrm.Web/Startup.cs
+++ SimpleCrm.Web/Startup.cs
@@ -32,7 +32,7 @@ namespace SimpleCrm.Web
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Happy Monday and have an awesome day!");
                 });
             });
         }
