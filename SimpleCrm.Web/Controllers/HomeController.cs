b73ace04cead284906721a2add3072d7d2354a1e
MikaZ21 <zukeyamamika@gmail.com>
2023-07-03 10:01:19 -0500

    M4: Step015 - Use the updated HomePageViewModel

--- SimpleCrm.Web/Controllers/HomeController.cs
+++ SimpleCrm.Web/Controllers/HomeController.cs
@@ -7,14 +7,19 @@ namespace SimpleCrm.Web.Controllers
 	public class HomeController : Controller 
 	{
 		private ICustomerData _customerData;
-		public HomeController(ICustomerData customerData)
+		private readonly IGreeter _greeter;
+
+		public HomeController(ICustomerData customerData, IGreeter greeter)
 		{
 			_customerData = customerData;
+			_greeter = greeter;
 		}
 
 		public IActionResult Index()
 		{
-			var model = _customerData.GetAll();
+			var model = new HomePageViewModel();
+			model.Customers = _customerData.GetAll();
+			model.CurrentMessage = _greeter.GetGreeting();
 			//var model = new CustomerModel
 			//{
 			// Id = 1,
