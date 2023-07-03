79869e92a8ecd63982ffef319dc9c855787d5512
MikaZ21 <zukeyamamika@gmail.com>
2023-07-03 06:08:14 -0500

    M3: Step014 - Display the list of customers

--- SimpleCrm.Web/Controllers/HomeController.cs
+++ SimpleCrm.Web/Controllers/HomeController.cs
@@ -6,18 +6,25 @@ namespace SimpleCrm.Web.Controllers
 {
 	public class HomeController : Controller 
 	{
+		private ICustomerData _customerData;
+		public HomeController(ICustomerData customerData)
+		{
+			_customerData = customerData;
+		}
+
 		public IActionResult Index()
 		{
-			var model = new CustomerModel
-			{
-			 Id = 1,
-			 FirstName = "Mika",
-			 LastName = "Zukeyama",
-			 PhoneNumber = "111-222-3333",
-			 Email = "aaa@gmail.com",
-			 FavoriteSeason = "Spring",
-			 FavoriteFlower = "Hydrangea"
-			};
+			var model = _customerData.GetAll();
+			//var model = new CustomerModel
+			//{
+			// Id = 1,
+			// FirstName = "Mika",
+			// LastName = "Zukeyama",
+			// PhoneNumber = "111-222-3333",
+			// Email = "aaa@gmail.com",
+			// FavoriteSeason = "Spring",
+			// FavoriteFlower = "Hydrangea"
+			//};
 
             //return View("Home",model);
             return View(model);
