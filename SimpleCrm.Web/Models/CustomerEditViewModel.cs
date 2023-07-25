d3f16364ea28a44e5f9508b9980937ee1ef1f6c9
MikaZ21 <zukeyamamika@gmail.com>
2023-07-26 01:17:27 +0900

    M4: Switch to EditorFor from TextboxFor

--- SimpleCrm.Web/Models/CustomerEditViewModel.cs
+++ SimpleCrm.Web/Models/CustomerEditViewModel.cs
@@ -7,15 +7,20 @@ namespace SimpleCrm.Web.Models
 	public class CustomerEditViewModel
 	{
 		public int Id { get; set; }
+
         [Display(Name = "First Name:")]
 		public string FirstName { get; set; }
+
         [Display(Name = "Last Name:")]
         public string LastName { get; set; }
+
 		[DataType(DataType.PhoneNumber)]
         [Display(Name = "Phone Number:")]
         public string PhoneNumber { get; set; }
+
         [Display(Name = "Option for news letter:")]
         public bool OptInNewsletter { get; set; }
+
         [Display(Name = "Type of customer:")]
         public CustomerType Type { get; set; }
 	}
