2f1bfb63257c6738a746c527c16d582ed37b66ea
MikaZ21 <zukeyamamika@gmail.com>
2023-08-05 20:16:12 -0500

    M5: Step023 EF Migrations, Re-Update

--- SimpleCrm/Customer.cs
+++ SimpleCrm/Customer.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace SimpleCrm
 {
 	public class Customer
 	{
 		public int Id { get; set; }
+
+		[MaxLength(50)]
+		[Required]
 		public string FirstName { get; set; }
-		public string LastName { get; set; }
+
+		[MinLength(1), MaxLength(50)]
+        [Required]
+        public string LastName { get; set; }
+
+		[MinLength(7), MaxLength(12)]
 		public string PhoneNumber { get; set; }
 		public bool OptInNewsletter { get; set; }
 		public CustomerType Type { get; set; }
