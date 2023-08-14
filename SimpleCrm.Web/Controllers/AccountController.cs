85c3d00df889718c25c3c1a3a7e6af343918923d
MikaZ21 <zukeyamamika@gmail.com>
2023-08-14 15:31:05 -0500

    M7: Complete the model and validation attributes

---
+++ SimpleCrm.Web/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleCrm.Web.Controllers
+{
+	public class AccountController : Controller
+	{
+		[HttpGet]
+		public IActionResult Register()
+		{
+			return View();
+		}
+	}
+}
+
