dd2c41110903ad77b2928e4099c99efca2726a2e
MikaZ21 <zukeyamamika@gmail.com>
2023-08-28 11:22:32 -0500

    M7: Step035 User registration -UserManager -CreateAsync -SignInManager -Adding ModelState Errors

--- SimpleCrm.Web/Controllers/AccountController.cs
+++ SimpleCrm.Web/Controllers/AccountController.cs
@@ -1,33 +1,52 @@
 ﻿using System;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SimpleCrm.Web.Models.Account;
 
 namespace SimpleCrm.Web.Controllers
 {
+
 	public class AccountController : Controller
 	{
-		[HttpGet]
+        private readonly UserManager<CrmUser> userManager;
+        private readonly SignInManager<CrmUser> signInManager;
+
+        public AccountController(UserManager<CrmUser> userManager, SignInManager<CrmUser> signInManager)
+        {
+            this.userManager = userManager;
+			this.signInManager = signInManager;
+        }
+
+
+        [HttpGet]
 		public IActionResult Register()
 		{
 			return View();
 		}
 
 		[HttpPost, ValidateAntiForgeryToken]
-		public IActionResult Register(RegisterUserViewModel model)
+		public async Task<IActionResult> Register(RegisterUserViewModel model)
 		{
 			if (ModelState.IsValid)
 			{
-				var RegisterUserView = new RegisterUserViewModel
+				var user = new CrmUser
 				{
 					UserName = model.UserName,
 					DisplayName = model.DisplayName,
-					Password = model.Password,
-					ConfirmPassword = model.ConfirmPassword
+					Email = model.UserName
 				};
-				
+				var createResult = await this.userManager.CreateAsync(user, model.Password);
+				if (createResult.Succeeded)
+				{
+					await this.signInManager.SignInAsync(user, isPersistent: false);
+					return RedirectToAction("Index", "Home");
+				}
+				foreach (var result in createResult.Errors)
+				{
+					ModelState.AddModelError("", result.Description);
+				}
 			}
 			return View();
 		}
 	}
 }
-
