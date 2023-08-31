a730893e549afae782b3875910a6b6b53164f0cc
MikaZ21 <zukeyamamika@gmail.com>
2023-08-31 09:52:41 -0500

    M7: Load the CrmUser into ViewComponent

---
+++ SimpleCrm.Web/ViewComponents/LoginLogoutViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleCrm.Web.ViewComponents
+
+{
+	public class LoginLogoutViewComponent : ViewComponent
+	{
+        private readonly UserManager<CrmUser> userManager;
+
+        public LoginLogoutViewComponent(UserManager<CrmUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+		{
+            var user = await this.userManager.FindByEmailAsync(this.User.Identity.Name);
+
+            
+			return View(user);
+		}
+	}
+}
+
