d2fb77a9a256fe1f9ed61aeb64fb44365f564492
MikaZ21 <zukeyamamika@gmail.com>
2023-09-04 16:15:55 -0500

    M8: Bundling into wwwroot NPM module files bundleconfig.json

---
+++ SimpleCrm.Web/bundleconfig.js
@@ -0,0 +1,30 @@
+﻿ [
+        {
+            "outputFileName": "wwwroot/js/vendor.min.js",
+            "inputFiles": [
+                "node_modules/material-design-lite/dist/material.min.js"
+            ],
+            "minify": {
+                "enabled": false
+            }
+        },
+        {
+            "outputFileName": "wwwroot/css/vendor.min.css",
+            "inputFiles": [
+                "node_modeles/material-design-lite/dist/material.amber-blue.min.css"
+            ],
+            "minify": {
+                "enabled": false
+            }
+        },
+        {
+            "outputFileName": "wwwroot/js/vendor-post.min.js",
+            "inputFiles": [
+                "node_modules/jquery/dist/jquery.min.js",
+                "node_modules/jquery-validation/dist/jquery.validate.min.js"
+            ],
+            "minify": {
+                "enabled": false
+            }
+        }
+ ]
