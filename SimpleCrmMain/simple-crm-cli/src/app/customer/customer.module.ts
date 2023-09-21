e74375cdfd0378e53116256d21534e3c10de3575
MikaZ21 <zukeyamamika@gmail.com>
2023-09-21 11:53:22 -0500

    M4: Step055 Http Service Definition customer service created

--- SimpleCrmMain/simple-crm-cli/src/app/customer/customer.module.ts
+++ SimpleCrmMain/simple-crm-cli/src/app/customer/customer.module.ts
@@ -5,6 +5,7 @@ import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerListPageComponent } from './customer-list-page/customer-list-page.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -13,9 +14,11 @@ import {MatTableModule} from '@angular/material/table';
   ],
   imports: [
     CommonModule,
+    HttpClientModule,
     CustomerRoutingModule,
     MatCardModule,
     MatTableModule
-  ]
+  ],
 })
+
 export class CustomerModule { }
