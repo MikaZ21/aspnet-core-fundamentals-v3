a9ee525e52907be36f4570eb0e53ac50a2e40a5f
MikaZ21 <zukeyamamika@gmail.com>
2023-10-09 11:30:32 -0500

    M6: Step064a Customer Detail Form Build the edit form

--- SimpleCrmMain/simple-crm-cli/src/app/customer/customer-detail/customer-detail.component.ts
+++ SimpleCrmMain/simple-crm-cli/src/app/customer/customer-detail/customer-detail.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Customer } from '../customer.model';
 import { CustomerService } from '../customer.service';
 import { Observable } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'crm-customer-detail',
@@ -13,12 +14,26 @@ import { Observable } from 'rxjs';
 export class CustomerDetailComponent implements OnInit {
   customerId: number;
   customer: Observable<Customer>;
+  detailForm!: FormGroup;
 
-  constructor(private route: ActivatedRoute,
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
     private customerService: CustomerService) {
       this.customerId = +this.route.snapshot.params['id'];
-      this.customer = this.customerService.get(this.customerId);    
+      this.customer = this.customerService.get(this.customerId);  
+      this.createForm();  
     }
+
+    public createForm(): void {
+                this.detailForm = this.fb.group({
+                  firstName: ['', Validators.required],
+                  lastName: ['', Validators.required], 
+                  phoneNumber: [''],
+                  emailAddress: ['', [Validators.required, Validators.email]],
+                  preferredContactMethod: ['email']
+                });
+  }
 
   ngOnInit(): void {
   }
