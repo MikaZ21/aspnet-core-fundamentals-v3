abea28966fb045d6e5e3f95a4e5041d27596e2d0
MikaZ21 <zukeyamamika@gmail.com>
2023-10-26 08:53:49 -0500

    M3: Step105a REST (MVC) Created controller with injected dependencies

---
+++ SimpleCrmMain/SimpleCrm.WebApi/ApiControllers/CustomerController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SimpleCrm.WebApi.ApiControllers
+{
+    [Route("api/customers")]
+    public class CustomerController : Controller
+    {
+        [Route("")]
+        public IActionResult GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPost("")]
+        public IActionResult Create([FromBody] Customer model)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Customer model)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
