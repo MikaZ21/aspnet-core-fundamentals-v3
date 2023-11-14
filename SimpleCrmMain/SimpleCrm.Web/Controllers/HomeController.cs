702f1849cb26f4ebf92cb65d5445eb6cffde1cef
MikaZ21 <zukeyamamika@gmail.com>
2023-11-13 21:40:39 -0600

    M6: Step110 Dynamic and Static Content Integrate Angular Builds - Angular build task configuration in .NET

--- SimpleCrmMain/SimpleCrm.Web/Controllers/HomeController.cs
+++ SimpleCrmMain/SimpleCrm.Web/Controllers/HomeController.cs
@@ -79,7 +79,7 @@ namespace SimpleCrm.Web.Controllers
 		public IActionResult Index()
 		{
 			var model = new HomePageViewModel();
-			model.Customers = _customerData.GetAll();
+			model.Customers = _customerData.GetAll(new CustomerListParameters { });
 			//model.CurrentMessage = _greeter.GetGreeting();
 			//var model = new CustomerModel
 			//{
