f73ba6c241a12203449c3f4691e1dada69e1cebd
MikaZ21 <zukeyamamika@gmail.com>
2023-11-18 16:23:25 -0600

    M6: Marketing Page Content

--- SimpleCrmMain/SimpleCrm.WebApi/Controllers/HomeController.cs
+++ SimpleCrmMain/SimpleCrm.WebApi/Controllers/HomeController.cs
@@ -2,31 +2,43 @@
 using Microsoft.AspNetCore.Mvc;
 using SimpleCrm.WebApi.Models;
 
-namespace SimpleCrm.WebApi.Controllers;
-
-public class HomeController : Controller
+namespace SimpleCrm.WebApi.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    [Route("")]
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        [Route("home")]
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        [ResponseCache(Duration = 30, Location = ResponseCacheLocation.Any)]
+        [Route("privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        [ResponseCache(Duration = 30, Location = ResponseCacheLocation.Any)]
+        [Route("corporate")]
+        public IActionResult CorporateClients()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [ResponseCache(Duration = 30, Location = ResponseCacheLocation.Any)]
+        [Route("pricing")]
+        public IActionResult Pricing()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("error")]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
-
