f028297d2cc5be671cf5932eaaf34f225fb80fec
MikaZ21 <zukeyamamika@gmail.com>
2023-12-10 19:26:45 -0600

    M8: Step116 JWT AuthController Create AuthController and CredentialsViewModel

---
+++ SimpleCrmMain/SimpleCrm.WebApi/ApiControllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using SimpleCrm.WebApi.Models.Auth;
+
+namespace SimpleCrm.WebApi.ApiControllers
+{
+	public class AuthController : Controller
+	{
+		[HttpPost("login")]
+		public async Task<IActionResult> Post([FromBody] CredentialsViewModel credentials)
+		{
+			if (!ModelState.IsValid)
+			{
+				return UnprocessableEntity(ModelState);
+			}
+
+			var user = await Authenticate(credentials.EmailAddress, credentials.Password);
+			if (user == null)
+			{
+				return UnprocessableEntity("Invalid username or password.");
+			}
+
+			var userModel = await GetUserData(user);
+			return Ok(userModel);
+		}
+	}
+}
+
---
+++ SimpleCrmMain/SimpleCrm.WebApi/Models/Auth/CredentialsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleCrm.WebApi.Models.Auth
+{
+	public class CredentialsViewModel
+	{
+		[Required]
+		public string EmailAddress { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
+
