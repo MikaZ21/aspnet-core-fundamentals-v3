3b6bd2b4b2e08eb08f194bf06417663160569b7c
MikaZ21 <zukeyamamika@gmail.com>
2024-05-02 20:11:17 -0500

    M4: Step162. Convert the Customer module to be lazy loaded. (Lazy load customer routes) Performance - Lazy Loading (Dela load secondary content)

--- SimpleCrmMain/simple-crm-cli/src/app/app-routing.module.ts
+++ SimpleCrmMain/simple-crm-cli/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+    // keep existing empty route
   {
     path: '',
     redirectTo: 'customers',
     pathMatch: 'full'
-  }
+  },  
+  {
+    path: 'customers',
+    loadChildren: () => import('./customer/customer.module').then(mod => mod.CustomerModule)
+  },
 ];
 
 @NgModule({
